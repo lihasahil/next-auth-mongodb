965ab5725b9985030a7b4984bb0581c6dcbd141b
lihasahil <sahilshrestha106@gmail.com>
2025-08-16 18:55:31 +0545

    feature: add signup route

--- src/dbConfig/db-config.ts
+++ src/dbConfig/db-config.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export async function connctDb() {
+export async function connectDb() {
   try {
     mongoose.connect(process.env.MONGO_URI!);
     const connection = mongoose.connection;
